095706604e953761ad107ecb1d11d8ed6ce17c76
Seres Dániel <seres.daniel20k@egressy.info>
2026-02-16 10:33:32 +0100

    Grade Model feltoltles

---
+++ BS_KretaProjekt/Model/GradeModel.cs
@@ -0,0 +1,14 @@
+﻿using BS_KretaProjekt.Persistence;
+
+namespace BS_KretaProjekt.Model
+{
+    public class GradeModel
+    {
+        private readonly KretaDbContext _context;
+        public GradeModel(KretaDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
