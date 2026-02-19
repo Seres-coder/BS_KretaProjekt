ecdf6b1d9c1874400ba361f24bd292fd55a157a4
Seres Dániel <seres.daniel20k@egressy.info>
2026-02-19 08:52:04 +0100

    GradeModel kesz

---
+++ BS_KretaProjekt/Dto/GradeModify.cs
@@ -0,0 +1,11 @@
+﻿namespace BS_KretaProjekt.Dto
+{
+    public class GradeModify
+    {
+
+        public int jegy_id { get; set; }
+        public int ertek { get; set; }
+        public string tantargy_nev { get; set; }
+        public string diak_nev { get; set; }
+    }
+}
--- BS_KretaProjekt/Model/GradeModel.cs
+++ BS_KretaProjekt/Model/GradeModel.cs
@@ -1,4 +1,7 @@
-﻿using BS_KretaProjekt.Persistence;
+﻿using System.Diagnostics;
+using BS_KretaProjekt.Dto;
+using BS_KretaProjekt.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace BS_KretaProjekt.Model
 {
@@ -9,6 +12,55 @@ namespace BS_KretaProjekt.Model
         {
             _context = context;
         }
-
+        #region Grade Add
+        public async Task AddNewGrade(GradeAdd dto)
+        {
+            using (var trx=_context.Database.BeginTransaction())
+            {
+                var tanarId=_context.Tanarok.First(x=>x.tanar_nev == dto.tanar_nev).tanar_id;
+                var tantargyId=_context.Tantargyok.First(x=>x.tantargy_nev==dto.tantargy_nev).tantargy_id;
+                var diakId=_context.Diakok.First(x=>x.diak_nev==dto.diak_nev).diak_id;
+                _context.Jegyek.Add(new Jegy
+                {
+                    datum=dto.datum,
+                    updatedatum=dto.updatedatum,
+                    ertek=dto.ertek,
+                    tantargy_id=tantargyId,
+                    diak_id=diakId,
+                    tanar_id=tanarId
+                });
+                _context.SaveChanges();
+                trx.Commit();
+            }
+            await Task.CompletedTask;
+        }
+        #endregion
+        #region -Grade Modify
+    
+        #endregion
+        #region -Grade Delete
+        public async Task DeleteGrade(int id)
+        {
+            using var trx=_context.Database.BeginTransaction();
+            _context.Remove(_context.Jegyek.First(x=>x.jegy_id==id));
+            _context.SaveChanges();
+            trx.Commit();
+        }
+        #endregion
+        #region -Grade Listing
+        public IEnumerable<GradeListDto> AllGrades()
+        {
+            return _context.Jegyek.Include(x => x.Tanar).Include(x => x.tantargy).Select(x => new GradeListDto
+            {
+                jegy_id = x.jegy_id,
+                datum = x.datum,
+                ertek = x.ertek,
+                updatedatum = x.updatedatum,
+                tanar_id = x.tanar_id,
+                tantargy_id = x.tantargy_id
+            }).ToList();
+               
+        }
+        #endregion
     }
 }
