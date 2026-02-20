ce3b44238d853909c06b0a885fa5eab845f4c10d
Seres.Daniel <Seres.Daniel@C113-13>
2026-02-20 14:25:13 +0100

    Model and Controller

---
+++ BS_KretaProjekt/Dto/TimeTableItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BS_KretaProjekt.Dto
+{
+    public class TimeTableItemDto
+    {
+        public int ora { get; set; }
+        public string tantargyNev { get; set; }
+        public string tanarNev { get; set; }
+    }
+}
---
+++ BS_KretaProjekt/Dto/UpdateOrarendDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BS_KretaProjekt.Dto
+{
+    public class UpdateOrarendDto
+    {
+        public int orarend_id { get; set; }
+        public string osztaly_nev { get; set; }
+        public DayOfWeek nap { get; set; }
+        public int ora { get; set; }
+        public string tantargy_nev { get; set; }
+        public string tanar_nev { get; set; }
+    }
+}
