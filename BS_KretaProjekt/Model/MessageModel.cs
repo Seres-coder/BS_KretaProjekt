2d124c0bcd4117e8f640ef3dad6fcd94cc6438c1
Seres Dániel <seres.daniel20k@egressy.info>
2026-03-24 11:58:55 +0100

    uzenetek javitgatasa

--- BS_KretaProjekt/Model/MessageModel.cs
+++ BS_KretaProjekt/Model/MessageModel.cs
@@ -38,20 +38,38 @@ namespace BS_KretaProjekt.Model
         #endregion
 
         #region Send Message
-        public IEnumerable<MessageDto> GetMessages(int user_id)
+        public IEnumerable<MessageDto> GetMessages(int fogado_id)
         {
-            if(!_context.Users.Any(x => x.user_id == user_id))
-                throw new KeyNotFoundException("nincs ilyen user");
+            if (!_context.Diakok.Any(x => x.diak_id == fogado_id))
+                throw new KeyNotFoundException("nincs ilyen diák");
 
-            return _context.Uzenetek.Include(x => x.User).Include(x => x.Fogado).Where(x => x.fogado_id == user_id).Select(x => new MessageDto
-            {
-                Id=x.uzenet_id,
-                cim = x.cim,
-                kuldoname = _context.Tanarok.Where(y => y.user_id == x.user_id).First().tanar_nev,
-                tartalom = x.tartalom,
-                kuldesidopontja=x.kuldesidopontja,
-            }).ToList().OrderByDescending(x => x.kuldesidopontja);
-
+            return _context.Uzenetek
+                .Include(x => x.User)
+                .Include(x => x.Fogado)
+                .Where(x => x.fogado_id == fogado_id)
+                .Select(x => new MessageDto
+                {
+                    Id = x.uzenet_id,
+                    cim = x.cim,
+                    kuldoname =
+                        _context.Tanarok
+                            .Where(t => t.user_id == x.user_id)
+                            .Select(t => t.tanar_nev)
+                            .FirstOrDefault()
+                        ?? _context.Diakok
+                            .Where(d => d.user_id == x.user_id)
+                            .Select(d => d.diak_nev)
+                            .FirstOrDefault()
+                        ?? _context.Users
+                            .Where(u => u.user_id == x.user_id)
+                            .Select(u => u.belepesnev)
+                            .FirstOrDefault()
+                        ?? "Ismeretlen feladó",
+                    tartalom = x.tartalom,
+                    kuldesidopontja = x.kuldesidopontja
+                })
+                .OrderByDescending(x => x.kuldesidopontja)
+                .ToList();
         }
         #endregion
 
