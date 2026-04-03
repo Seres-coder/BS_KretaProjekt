db0cbb96afb766d089c12455d61a48579216648d
Norman <bago.norman20@egressy.info>
2026-04-03 16:50:27 +0200

    Uzenetek

--- KretaAv/KretaAv/Model/UzenetekModel.cs
+++ KretaAv/KretaAv/Model/UzenetekModel.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
+using KretaAv.Dto;
 
 namespace KretaAv.Model
 {
-    internal class UzenetekModel
+    public class UzenetekModel
     {
+        public ApiSession _session;
+
+        public UzenetekModel(ApiSession session)
+        {
+            _session = session;
+        }
+
+        public async Task<List<UzenetekDto>> GetUzenetek()
+        {
+            var result = await _session.Client.GetFromJsonAsync<List<UzenetekDto>>("api/uzenet/messageklistazasa");
+            return result;
+        }
+
+        public async Task<CreateMessageDto> AddNewMessage(CreateMessageDto dto)
+        {
+            var response = await _session.Client.PostAsJsonAsync("api/uzenet/messageadd", dto);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadFromJsonAsync<CreateMessageDto>();
+            return result;
+        }
+
+        public class Uzenet
+        {
+            public string Szoveg { get; set; } = "";
+        }
     }
 }
