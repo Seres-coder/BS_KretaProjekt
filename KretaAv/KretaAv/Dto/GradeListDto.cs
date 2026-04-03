fd7c65423f01538144d0ff6a61d54357320a44a0
Norman <bago.norman20@egressy.info>
2026-04-03 16:32:58 +0200

    avalonia dto-k adatokkal

---
+++ KretaAv/KretaAv/Dto/GradeListDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KretaAvalonia.Dto
+{
+    public class GradeListDto
+    {
+        public int jegy_id { get; set; }
+        public int diak_id { get; set; }
+        public DateTimeOffset datum { get; set; }
+        public DateTimeOffset updatedatum { get; set; }
+        public int ertek { get; set; }
+        public int tantargy_id { get; set; }
+        public int tanar_id { get; set; }
+
+        public string tantargyNev { get; set; }
+        public string tanarNev { get; set; }
+    }
+}
