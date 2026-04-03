0f3009a96222e9d690d3ae418db69e88de23eb34
Norman <bago.norman20@egressy.info>
2026-04-03 16:52:08 +0200

    Tantargyak

--- KretaAv/KretaAv/ViewModels/TantargyatlagViewModel.cs
+++ KretaAv/KretaAv/ViewModels/TantargyatlagViewModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KretaAv.Dto;
 
 namespace KretaAv.ViewModels
 {
-    internal class TantargyatlagViewModel
+    public class TantargyatlagViewModel
     {
+        private string _name;
+        private List<GradeListDto> jegyek;
+
+
+        public string Name { get { return _name; } }
+        public double Avg { get { return avgcounter(); } }
+        private double avgcounter()
+        {
+            double sum = 0;
+
+            foreach (GradeListDto item in jegyek)
+            {
+                sum += item.ertek;
+            }
+            return sum / jegyek.Count;
+        }
+        public TantargyatlagViewModel(string name, List<GradeListDto> dto)
+        {
+            _name = name;
+            jegyek = dto;
+        }
     }
 }
