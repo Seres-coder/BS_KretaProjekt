7a49a3a5650d2e2c7ce889f654eb9cbdbdd0a065
Norman <bago.norman20@egressy.info>
2026-04-03 16:38:23 +0200

    avalonia apisession es authmodel elkeszitese

---
+++ KretaAv/KretaAv/Dto/LoginResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KretaAv.Dto
+{
+    public class LoginResponseDto
+    {
+        
+            public int id { get; set; }
+            public string name { get; set; } = "";
+            public string role { get; set; } = "";
+        
+    }
+}
--- KretaAv/KretaAv/Model/AuthModel.cs
+++ KretaAv/KretaAv/Model/AuthModel.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
+using KretaAv.Dto;
+
 
 namespace KretaAv.Model
 {
-    internal class AuthModel
+    public class AuthModel
     {
+        public ApiSession _session;
+
+        public AuthModel(ApiSession session)
+        {
+            _session = session;
+        }
+
+        public async Task<HttpResponseMessage> Login(Dto.UserDto dto)
+        {
+           
+            var res = await _session.Client.PostAsync(
+                $"api/User/login?username={Uri.EscapeDataString(dto._belepesnev)}&password={Uri.EscapeDataString(dto._jelszo)}",
+                null);
+
+            if (!res.IsSuccessStatusCode)
+                return res;
+
+            
+            var user = await res.Content.ReadFromJsonAsync<LoginResponseDto>();
+            if (user == null)
+                throw new InvalidOperationException("Nem sikerült beolvasni a bejelentkezési választ.");
+
+            _session.Userid = user.id;
+            _session.Username = user.name;
+            _session.Role = user.role;
+
+            return res;
+        }
+
+        public async Task Logout()
+        {
+            var res = await _session.Client.PostAsync("api/User/logout", null);
+            res.EnsureSuccessStatusCode();
+
+            _session.Userid = 0;
+            _session.Username = "";
+            _session.Role = "";
+        }
+
+        
     }
 }
