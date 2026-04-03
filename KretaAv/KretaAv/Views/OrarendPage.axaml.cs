0554c0755e4113d82b5e50ac4f879c8b4a873654
Norman <bago.norman20@egressy.info>
2026-04-03 16:25:58 +0200

    avalonia

---
+++ KretaAv/KretaAv/Views/OrarendPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KretaAv;
+
+public partial class OrarendPage : UserControl
+{
+    public OrarendPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ KretaAv/KretaAv/Views/Tantargyatlag.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KretaAv;
+
+public partial class Tantargyatlag : UserControl
+{
+    public Tantargyatlag()
+    {
+        InitializeComponent();
+    }
+}
