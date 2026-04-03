d7de54876fcbff60bb5a4b240ef4ad0fed6e07f9
Norman <bago.norman20@egressy.info>
2026-04-03 15:19:58 +0200

    avalonia egeszbe

---
+++ KretaAvalonia/KretaAvalonia/ViewModels/TantargyatlagViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KretaAvalonia.Dto;
+
+namespace KretaAvalonia.ViewModels
+{
+    public class TantargyatlagViewModel
+    {
+        private string _name;
+        private List<GradeListDto> jegyek;
+        
+       
+        public string Name { get { return _name; } }
+        public double Avg { get { return avgcounter(); } }
+        private double avgcounter()
+        {
+            double sum = 0;
+
+            foreach (GradeListDto item in jegyek)
+            {
+                sum += item.ertek;
+            }
+            return sum/jegyek.Count;
+        }
+        public TantargyatlagViewModel(string name,List<GradeListDto> dto)
+        {
+            _name = name;
+            jegyek = dto;
+        }
+    }
+}
---
+++ KretaAvalonia/KretaAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace KretaAvalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
